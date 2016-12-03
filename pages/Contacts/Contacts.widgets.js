94db745cbee3c33862a94973ec780d3b58314929
cesar <cesarechazu@gmail.com>
2016-12-03 15:27:47 -0300

    Modificacion desde Sony Vaio

--- pages/Contacts/Contacts.widgets.js
+++ pages/Contacts/Contacts.widgets.js
@@ -4,20 +4,19 @@ Contacts.widgets = {
 	}],
 	layoutBox1: ["wm.Layout", {"horizontalAlign":"left","verticalAlign":"top"}, {}, {
 		searchText: ["wm.Text", {"border":"0","caption":"Search","captionSize":"60px","dataValue":undefined,"displayValue":"","height":"42px","width":"100%"}, {}],
-		contactsList: ["wm.List", {"_classes":{"domNode":["MobileListStyle"]},"border":"0","columns":[
-{"show":true,"field":"id","title":"Id","width":"80px","displayType":"Number","align":"left","formatFunc":""},
-{"show":true,"field":"name.formatted","title":"Name.formatted","width":"100%","displayType":"String","align":"left","formatFunc":""},
-{"show":true,"field":"name.familyName","title":"Name.familyName","width":"100%","displayType":"String","align":"left","formatFunc":""},
-{"show":true,"field":"name.givenName","title":"Name.givenName","width":"100%","displayType":"String","align":"left","formatFunc":""},
-{"show":true,"field":"name.middleName","title":"Name.middleName","width":"100%","displayType":"String","align":"left","formatFunc":""},
-{"show":true,"field":"name.honorificPrefix","title":"Name.honorificPrefix","width":"100%","displayType":"String","align":"left","formatFunc":""},
-{"show":true,"field":"name.honorificSuffix","title":"Name.honorificSuffix","width":"100%","displayType":"String","align":"left","formatFunc":""},
-{"show":true,"field":"nickname","title":"Nickname","width":"100%","displayType":"String","align":"left","formatFunc":""},
-{"show":true,"field":"birthday","title":"Birthday","width":"100%","displayType":"Java.util.Date","align":"left","formatFunc":""},
-{"show":true,"field":"note","title":"Note","width":"100%","displayType":"String","align":"left","formatFunc":""},
-{"show":false,"field":"PHONE COLUMN","title":"-","width":"100%","align":"left","expression":"\"<div class='MobileRowTitle'>\" +\n\"Id: \" + ${id} +\n\"</div>\"\n\n+ \"<div class='MobileRow'>\" +\n\"Name.formatted: \" + ${name.formatted}\n + \"</div>\"\n\n+ \"<div class='MobileRow'>\" +\n\"Name.familyName: \" + ${name.familyName}\n + \"</div>\"\n\n+ \"<div class='MobileRow'>\" +\n\"Name.givenName: \" + ${name.givenName}\n + \"</div>\"\n\n+ \"<div class='MobileRow'>\" +\n\"Name.middleName: \" + ${name.middleName}\n + \"</div>\"\n\n+ \"<div class='MobileRow'>\" +\n\"Name.honorificPrefix: \" + ${name.honorificPrefix}\n + \"</div>\"\n\n+ \"<div class='MobileRow'>\" +\n\"Name.honorificSuffix: \" + ${name.honorificSuffix}\n + \"</div>\"\n\n+ \"<div class='MobileRow'>\" +\n\"Nickname: \" + ${nickname}\n + \"</div>\"\n\n+ \"<div class='MobileRow'>\" +\n\"Birthday: \" + ${birthday}\n + \"</div>\"\n\n+ \"<div class='MobileRow'>\" +\n\"Note: \" + ${note}\n + \"</div>\"\n\n","mobileColumn":true},
-{"show":true,"controller":"rightarrow","width":"20px","title":"-","field":"_rightArrow","mobileColumn":true}
-],"headerVisible":false,"height":"100%","margin":"0","minDesktopHeight":60,"rightNavArrow":true,"selectFirstRow":true,"styleAsGrid":false}, {}, {
+		contactsList: ["wm.List", {"border":"0","columns":[
+{"show":true,"field":"id","title":"Id","width":"80px","align":"left","formatFunc":"","mobileColumn":false},
+{"show":true,"field":"name.formatted","title":"Name","width":"100px","align":"left","formatFunc":"","mobileColumn":false},
+{"show":true,"field":"name.familyName","title":"Family Name","width":"100px","align":"left","formatFunc":"","mobileColumn":false},
+{"show":true,"field":"name.givenName","title":"Given Name","width":"100px","align":"left","formatFunc":"","mobileColumn":false},
+{"show":true,"field":"name.middleName","title":"Middle Name","width":"100px","align":"left","formatFunc":"","mobileColumn":false},
+{"show":true,"field":"name.honorificPrefix","title":"Honorific Prefix","width":"120px","align":"left","formatFunc":"","mobileColumn":false},
+{"show":true,"field":"name.honorificSuffix","title":"Honorific Suffix","width":"120px","align":"left","formatFunc":"","mobileColumn":false},
+{"show":true,"field":"nickname","title":"NickName","width":"100px","align":"left","formatFunc":"","mobileColumn":false},
+{"show":true,"field":"birthday","title":"Birthday","width":"110px","align":"left","formatFunc":"","mobileColumn":false},
+{"show":true,"field":"note","title":"Note","width":"100px","align":"left","formatFunc":"","mobileColumn":false},
+{"show":false,"field":"PHONE COLUMN","title":"-","width":"100%","align":"left","expression":"\"<div class='MobileRowTitle'>\" +\n\"Id: \" + ${id} +\n\"</div>\"\n\n+ \"<div class='MobileRow'>\" +\n\"Name: \" + ${name.formatted}\n + \"</div>\"\n\n+ \"<div class='MobileRow'>\" +\n\"Family Name: \" + ${name.familyName}\n + \"</div>\"\n\n+ \"<div class='MobileRow'>\" +\n\"Given Name: \" + ${name.givenName}\n + \"</div>\"\n\n+ \"<div class='MobileRow'>\" +\n\"Middle Name: \" + ${name.middleName}\n + \"</div>\"\n\n+ \"<div class='MobileRow'>\" +\n\"Honorific Prefix: \" + ${name.honorificPrefix}\n + \"</div>\"\n\n+ \"<div class='MobileRow'>\" +\n\"Honorific Suffix: \" + ${name.honorificSuffix}\n + \"</div>\"\n\n+ \"<div class='MobileRow'>\" +\n\"NickName: \" + ${nickname}\n + \"</div>\"\n\n+ \"<div class='MobileRow'>\" +\n\"Birthday: \" + ${birthday}\n + \"</div>\"\n\n+ \"<div class='MobileRow'>\" +\n\"Note: \" + ${note}\n + \"</div>\"\n\n","mobileColumn":false}
+],"headerVisible":false,"height":"100%","margin":"0","minDesktopHeight":60,"selectFirstRow":true}, {}, {
 			binding: ["wm.Binding", {}, {}, {
 				wire: ["wm.Wire", {"expression":undefined,"source":"phoneGapCallContacts","targetProperty":"dataSet"}, {}]
 			}]
