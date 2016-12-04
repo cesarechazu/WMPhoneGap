7f0609a14aa2a847aec1af8f1402decc8a87504c
cesar <cesarechazu@gmail.com>
2016-12-03 22:01:39 -0300

    Modificacion desde Sony Vaio

--- pages/CameraPhoto/CameraPhoto.a.js
+++ pages/CameraPhoto/CameraPhoto.a.js
@@ -10,11 +10,11 @@ phoneGapCallPhotoError: function(inSender, inError) {
 app.alert(inError.toString());
 },
 takePhotoJSButtonClick: function(inSender) {
-navigator.camera.getPicture(onSuccess,
-onFail, {quality: 50,destinationType: Camera.DestinationType.FILE_URI});
+navigator.camera.getPicture(onSuccess, onFail, {quality: 50,destinationType: Camera.DestinationType.FILE_URI});
 function onSuccess(imageURI) {
 //var image = document.getElementById('myImage');
 //image.src = imageURI;
+app.toastInfo("Photo taken", 2500);
 this.picture1.setValue("source",null);
 this.picture1.setValue("source",imageURI);
 }
